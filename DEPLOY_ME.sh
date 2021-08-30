e28f57fbf1307046ae6a2ca2798c6c779eb17e9c
vilius.kukanauskas <vilius.kukanauskas@gmail.com>
2021-08-30 19:08:27 +0200

    removed comments from default skelleton

--- DEPLOY_ME.sh
+++ DEPLOY_ME.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#first you have to build the artefact, for example gradle or mvn
+#gradle clean build  or mvn clean build
+
 #here you can get the values for deployment variables from secret store like parameter store as example
 export REGION="eu-central-1"
-
 serverless deploy
---
+++ README.md
@@ -0,0 +1,5 @@
+This program filters bucket and filekey from input in LambdaHandler,
+
+gets the terraform state with those bucket / key
+
+and then returns the outputs from that state as Json
